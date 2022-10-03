a9fc6eb3ba439aec99861d1b3902eb54ace712cc
unknown <hgh21233@gmail.com>
2022-10-04 01:17:23 +0900

    a

--- testtt.py
+++ testtt.py
@@ -1,27 +1,8 @@
-import sys
-input = sys.stdin.readline
-def hm(hl) :
-    for i in range(100-hl+1) :
-        for j in range(100) :
-            p, q = g[j][i:i+hl], s[j][i:i+hl]
-            if p == p[::-1] :
-                return True
-            if q == q[::-1] :
-                return True
-    return False
-
-for t in range(1, 11) :
-    _ = input()
-    g = [input() for _ in range(100)]
-    s = [''.join(x) for x in zip(*g)]
-
-    for i in range(100, 0, -1) :
-        if hm(i):
-            l = i
-            break
-    print(f"#{t} {l}")
-
-    . . v . .
-    . . . < @
-    ^ . > . .
-    > . . v .
+arr = [2, 3, 4]
+subsets = [[]]
+for num in arr:
+    print(num)
+    size = len(subsets)
+    for y in range(size):
+        subsets.append(subsets[y]+[num])
+print(subsets)
