d27b871be36aac83c53d854591ade1639d5078bc
HanKyeon <hgh21233@gmail.com>
2023-05-28 14:12:38 +0900

    일일알고

---
+++ BAEKJOON/11055.py
@@ -0,0 +1,31 @@
+'''
+가장 큰 증가하는 부분 수열
+
+증가하는 부분 수열 중 합이 가장 큰 것을 구해라.
+
+입력
+수열 크기 n 제시.
+수열 제시.
+
+출력
+가장 큰 증가하는 부분 수열의 합 출력
+'''
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+nl = list(map(int, input().rstrip().split()))
+dp = nl[:]
+
+for i in range(n-1):
+    num = nl[i]
+    for j in range(i, n):
+        num2 = nl[j]
+        if num < num2 and dp[j] < dp[i]+num2:
+            dp[j] = dp[i]+num2
+
+print(max(dp))
+
+
+
+
