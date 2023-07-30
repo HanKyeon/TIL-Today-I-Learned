14ab503e8811f746b1df298ae0b97879f8bee872
HanKyeon <hgh21233@gmail.com>
2023-07-30 16:08:58 +0900

    일일알고

--- test.py
+++ test.py
@@ -1,13 +1,24 @@
 # import sys
 # input = sys.stdin.readline
 
-for _ in range(int(input())):
-    k, n = int(input()), int(input())
-    dp = [[i for i in range(n+1)]] + [[0]*(n+1) for _ in range(k)]
-    for i in range(1, k+1):
-        for j in range(1, n+1):
-            dp[i][j] = dp[i-1][j]+dp[i][j-1]
-    print(dp[-1][-1])
+di = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}
+while True:
+    s = input().rstrip()
+    if s == '#':
+        break
+    ans = 0
+    for i in s:
+        if i in di:
+            ans += 1
+    print(ans)
+
+# for _ in range(int(input())):
+#     k, n = int(input()), int(input())
+#     dp = [[i for i in range(n+1)]] + [[0]*(n+1) for _ in range(k)]
+#     for i in range(1, k+1):
+#         for j in range(1, n+1):
+#             dp[i][j] = dp[i-1][j]+dp[i][j-1]
+#     print(dp[-1][-1])
 
 
 # while True:
