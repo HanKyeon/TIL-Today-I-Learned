df45af7dca3b04f0e2de9d9243c729b010fd6992
HanKyeon <hgh21233@gmail.com>
2023-10-28 13:42:15 +0900

    일일알고

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 import sys
 input = sys.stdin.readline
 
+n=int(input())
+for i in range(n,0,-1): print(" "*(n-i)+"*"*(2*i-1))
+
 while 1:
     n, a, w = input().split()
     if not int(a)+int(w): break
