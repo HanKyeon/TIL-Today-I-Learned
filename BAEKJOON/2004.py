ecb69b981387fad71c64b1c9e45762c0f0d345e5
HanKyeon <hgh21233@gmail.com>
2023-12-12 22:11:32 +0900

    일일알고

---
+++ BAEKJOON/2004.py
@@ -0,0 +1,28 @@
+'''
+조합 0의 갯수
+
+(n m)의 끝자리 0의 갯수를 출력해라.
+
+입력
+n, m 제시
+
+출력
+끝자리 0의 갯수 출력
+'''
+n, m = map(int, input().split())
+
+def two_count(n):
+    two = 0
+    while n != 0:
+        n = n // 2
+        two += n
+    return two
+
+def five_count(n):
+    five = 0
+    while n != 0:
+        n = n // 5
+        five += n
+    return five
+
+print(min(two_count(n)-two_count(n-m)-two_count(m), five_count(n)-five_count(n-m)-five_count(m)))
